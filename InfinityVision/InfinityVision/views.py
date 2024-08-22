26b038dec35771bd202cb520d9554b5c54a36e34
BogSyn <ravlykplus@gmail.com>
2024-08-22 23:18:45 +0300

    add description

--- InfinityVision/InfinityVision/views.py
+++ InfinityVision/InfinityVision/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'InfinityVision/index.html')
+
+
+def about(request):
+    return render(request, 'InfinityVision/about.html')
